91bd3843e9248bc1505602baabad3b5ebc275d1d
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2024-09-20 19:06:12 -0500

    database connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+
+// Set up Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
+// GraphQL query to get the message
+const GET_MESSAGE = gql`
+  query GetMessage {
+    message
+  }
+`;
+
+function Message() {
+  const { loading, error, data } = useQuery(GET_MESSAGE);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!!!</p>;
+
+  return <h1>{data.message}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Art Storefront</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Message />
+      </div>
+    </ApolloProvider>
   );
 }
 
